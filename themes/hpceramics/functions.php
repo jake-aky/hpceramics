eda83c91d20623c00f6cca2edfeff3cf1e12e22f
Jake Atkinson <jakeatkinson@Jakes-MacBook-Pro.local>
2024-11-14 11:36:22 +0000

    feat: added mandatory theme files

---
+++ themes/hpceramics/functions.php
@@ -0,0 +1,9 @@
+<?php
+function portfolio_enqueue_styles() {
+    // Enqueue the main stylesheet
+    wp_enqueue_style('main-style', get_stylesheet_uri(),array(), rand(111,9999));
+}
+add_action('wp_enqueue_scripts', 'portfolio_enqueue_styles');
+
+/* Disable WordPress Admin Bar for all users */
+add_filter( 'show_admin_bar', '__return_false' );
