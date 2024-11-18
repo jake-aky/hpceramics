690266b8debb1eff3d735c8a278c63816589943c
Jake Atkinson <jakeatkinson@Jakes-MacBook-Pro.local>
2024-11-18 14:44:25 +0000

    feat: added test plugin to fix showing homepage before modal runs

---
+++ plugins/svg-modal/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'b22c904232d1e98506b8');
---
+++ plugins/svg-modal/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '84f19082ce956fe9925a', 'type' => 'module');
