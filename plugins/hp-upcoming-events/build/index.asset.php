bc33da53d76e5494e2f5b8329f283fad762ca0db
Jake Atkinson <jakeatkinson@Jakes-MacBook-Pro.local>
2024-11-19 15:01:46 +0000

    feat: removed hp-upcoming-events plugin

--- plugins/hp-upcoming-events/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e2b788ec28d6368deb9d');
--- plugins/hp-upcoming-events/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '8336dca1c6e686161a7c', 'type' => 'module');
